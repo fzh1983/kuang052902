6ae1e93aa5f00a9938973d3828d61572c2229d3c
“itcast” <hello@itcast.cn>
2022-05-30 09:45:13 +0800

    init:初始化项目

---
+++ src/com/kuang/Demo.java
@@ -0,0 +1,9 @@
+package com.kuang;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+        System.out.println("2022年5月30日09:43:50");
+
+    }
+}
